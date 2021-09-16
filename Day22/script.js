fbd04e65053259fd24217155c755027744f08c39
Sophie <sophiehickey123@gmail.com>
2021-09-16 21:23:34 +0100

    day22 added

---
+++ Day22/script.js
@@ -0,0 +1,20 @@
+const triggers = document.querySelectorAll("a");
+const highlight = document.createElement("span");
+highlight.classList.add("highlight");
+document.body.appendChild(highlight);
+
+function highlightLink() {
+  const linkCoords = this.getBoundingClientRect();
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX,
+  };
+
+  highlight.style.width = `${linkCoords.left}px`;
+  highlight.style.width = `${linkCoords.right}px`;
+  highlight.style.transform = `translate(${coords.left}px,${coords.top}px)`;
+}
+
+triggers.forEach((a) => a.addEventListener("mouseenter", highlightLink));
